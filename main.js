904935bea7526b8856a5d159b3794d98f1c9f93f
Raghavendra4348 <63504504+Raghavendra4348@users.noreply.github.com>
2022-07-03 01:03:59 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,54 @@
+function setFormMessage(formElement, type, message) {
+    const messageElement = formElement.querySelector(".form__message");
+
+    messageElement.textContent = message;
+    messageElement.classList.remove("form__message--success", "form__message--error");
+    messageElement.classList.add(`form__message--${type}`);
+}
+
+function setInputError(inputElement, message) {
+    inputElement.classList.add("form__input--error");
+    inputElement.parentElement.querySelector(".form__input-error-message").textContent = message;
+}
+
+function clearInputError(inputElement) {
+    inputElement.classList.remove("form__input--error");
+    inputElement.parentElement.querySelector(".form__input-error-message").textContent = "";
+}
+
+document.addEventListener("DOMContentLoaded", () => {
+    const SignInForm = document.querySelector("#SignIn");
+    const signUpnowForm = document.querySelector("#signUpnow");
+
+    document.querySelector("#linkSignUpnow").addEventListener("click", e => {
+        e.preventDefault();
+        SignInForm.classList.add("form--hidden");
+        signUpnowForm.classList.remove("form--hidden");
+    });
+
+    document.querySelector("#linkSignIn").addEventListener("click", e => {
+        e.preventDefault();
+        SignInForm.classList.remove("form--hidden");
+        signUpnowForm.classList.add("form--hidden");
+    });
+
+    SignInForm.addEventListener("submit", e => {
+        e.preventDefault();
+
+        // Perform your AJAX/Fetch login
+
+        setFormMessage(loginForm, "error", "Invalid username/password combination");
+    });
+
+    document.querySelectorAll(".form__input").forEach(inputElement => {
+        inputElement.addEventListener("blur", e => {
+            if (e.target.id === "signupUsername" && e.target.value.length > 0 && e.target.value.length < 10) {
+                setInputError(inputElement, "Username must be at least 10 characters in length");
+            }
+        });
+
+        inputElement.addEventListener("input", e => {
+            clearInputError(inputElement);
+        });
+    });
+});
